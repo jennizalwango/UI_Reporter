26391f4000b3877859aaa5059abfa98e93dc290e
jennizalwango <jennyzalwango12@gmail.com>
2019-01-05 15:07:31 +0300

    change file from routes.py to incident_veiws.py

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -7,20 +7,18 @@ class APITestCase(BaseTestCase):
     def test_register_user(self):
        with self.client:
             user = {
-                "username" : "jenny",
-                "email": "jenny@gmail.com", 
-                "password": "password",
-                "first_name": "zawal",
-                "last_name": "jenni",
-                "other_names":"deal",
-                "phone_number": "0708494848", 
-                "is_admin": False,
+                    "username":"moses",
+                    "password":"word",
+                    "email":"jennyiceuy@gmail.com",
+                    "first_name":"zawah",
+                    "last_name":"boy",
+                    "other_names":"sweets",
+                    "phone_number":"0706237809",
+                    "is_admin": ""
             }
             response = self.client.post('/api/v1/register', data=json.dumps(user), content_type='application/json')
-            data = json.loads(response.data.decode())
-            self.assertEqual("User account created successfully", data["message"])
-            self.assertEqual(201, response.status_code)
-
+            self.assertEqual(response.status,'201 CREATED')
+         
     def test_required_fields(self):
         with self.client:
             response = self.client.post('/api/v1/register', data=json.dumps({}), content_type='application/json')
@@ -31,30 +29,7 @@ class APITestCase(BaseTestCase):
 
     def test_login_user_sucessful(self):
         with self.client:
-            user = {
-                "username":"jenny",
-                "password": "12345"
-            }
-            response = self.client.post('/api/v1/login', data=json.dumps(user), content_type='application/json')
-            data = json.loads(response.data.decode())
-            self.assertEqual("Login Successful", data['message'])
-            self.assertEqual(200, response.status_code)
-
-    def test_login_invalid(self):
-        with self.client:
-            login_data ={
-                "username": "jenny",
-                "password": "12345"
-            }
-            response = self.client.post('/api/v1/login', data=json.dumps(login_data),content_type='application/json')
-            results = json.loads(reponse.data.decode())
-            print(results)
-            self.assertEqual("Invalid username or password",results["error"])
-            self.assertEqual(400, response.stauts_code)
-
-    def test_created_incident(self):
-        with self.client:
-            create_user = {
+            users = {
                 "username" : "jenny",
                 "email": "jenny@gmail.com", 
                 "password": "password",
@@ -64,7 +39,51 @@ class APITestCase(BaseTestCase):
                 "phone_number": "0708494848", 
                 "is_admin": False,
             }
+            self.client.post('/api/v1/register', data=json.dumps(users),content_type='application/json') 
 
+            user = {
+                "username":"jenny",
+                "password": "password"
+            }
+            
+            response = self.client.post('api/v1/login', data=json.dumps(user), content_type='application/json')
+            self.assertEqual(response.status_code,200)
+
+    def test_login_invalid(self):
+        with self.client:
+            users = {
+                "username" : "jenny",
+                "email": "jenny@gmail.com", 
+                "password": "password",
+                "first_name": "zawal",
+                "last_name": "jenni",
+                "other_names":"deal",
+                "phone_number": "0708494848", 
+                "is_admin": False
+            }
+            self.client.post('/api/v1/register', data=json.dumps(users),content_type='application/json') 
+
+            login_data ={
+                "username": "jenny",
+                "password": "12345"
+            }
+            response = self.client.post('/api/v1/login', data=json.dumps(login_data),content_type='application/json')
+            self.assertEqual(response.status,'400 BAD REQUEST')
+            
+
+    def test_create_incident(self):
+        with self.client:
+            create_user = {
+                "username" : "jenny",
+                "email": "jenny@gmail.com", 
+                "password": "password",
+                "first_name": "zawal",
+                "last_name": "jenni",
+                "other_names":"deal",
+                "phone_number": "0708494848", 
+                "is_admin": False
+            }
+            self.client.post('/api/v1/register', data=json.dumps(create_user),content_type='application/json')
             incident_data = {
                 "created_by": "jenny", 
                 "incident_type": "redflag",
@@ -73,13 +92,10 @@ class APITestCase(BaseTestCase):
                 "status": "draft",
                 "images":"come.jpg",  
                 "videos":"go.png",
-                "comment":"fair",
+                "comment":"fair"
             }
             response = self.client.post('/api/v1/incident', data=json.dumps(incident_data), content_type='application/json')
-            data = json.loads(response.data.decode())
-            print(data)
-            self.assertEqual("Incident created successfully", data["message"])
-            self.assertEqual(201, response.status_code)
+            self.assertEqual(response.status_code, 201)
 
     def test_user_not_exits(self):
         with self.client:
@@ -94,7 +110,36 @@ class APITestCase(BaseTestCase):
                 "comment": "nice",
             }
             response = self.client.post('/api/v1/incident', data=json.dumps(incident_data), content_type='application/json')
-            data = json.loads(response.data.decode())
             self.assertEqual(400, response.status_code)
-            self.assertEqual("User assigned doesnot exist", data["errors"])
             
+    def test_get_specific_incident(self):
+        with self.client:
+            incident_data = {
+                "created_by": "jenny", 
+                "incident_type": "redflag",
+                "location": "bukoto",
+                "phone_number": "0726725271",
+                "status": "draft",
+                "images":"come.jpg",  
+                "videos":"go.png",
+                "comment":"fair"
+            }
+            response = self.client.post('/api/v1/incident', data=json.dumps(incident_data), content_type='application/json')
+            self.assertEqual(response.status_code, 201)
+
+    def test_fetch_all_incident(self):
+        with self.client:
+            incident_data = {
+                "created_by": "jenny", 
+                "incident_type": "redflag",
+                "location": "bukoto",
+                "phone_number": "0726725271",
+                "status": "draft",
+                "images":"come.jpg",  
+                "videos":"go.png",
+                "comment":"fair"
+            }
+            response = self.client.post('/api/v1/incident', data=json.dumps(incident_data), content_type='application/json')
+            self.assertEqual(response.status_code, 201)
+
+    def test_
