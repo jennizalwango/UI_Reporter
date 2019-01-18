c2b955d868341f5be7feee4f973807bf017b24cf
jennizalwango <jennyzalwango12@gmail.com>
2019-01-18 12:12:44 +0300

    Create user in the set up function

--- app/tests/base.py
+++ app/tests/base.py
@@ -1,5 +1,3 @@
-
-
 from unittest import TestCase
 from app import create_app
 from app.views import incident_views 
@@ -11,6 +9,16 @@ class BaseTestCase(TestCase):
     self.app_context = self.app.app_context()
     self.app_context.push()
     self.client = self.app.test_client()
+    self.create_user = {
+                "username" : "jenny",
+                "email": "jenny@gmail.com", 
+                "password": "password",
+                "first_name": "zawal",
+                "last_name": "jenni",
+                "other_names":"deal",
+                "phone_number": "0708494848", 
+                "is_admin": False
+            } 
 
   def tearDown(self):
     self.app = self.app_context.pop()
