0637b0f3caf8f06c49560a7886e4f08daeeb7999
jennizalwango <jennyzalwango12@gmail.com>
2019-01-19 14:16:00 +0300

    Write more tests

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -20,7 +20,7 @@ class APITestCase(BaseTestCase):
             results = json.loads(response.data.decode())
             print(results)
             self.assertEqual("Please provide the required fields", results["error"])
-            self.assertEqual(404, response.status_code)
+            self.assertEqual(400, response.status_code)
 
     def test_register_user_already_exists(self):
        with self.client:
@@ -45,14 +45,14 @@ class APITestCase(BaseTestCase):
 
     def test_login_invalid(self):
         with self.client:
-            self.client.post('/api/v1/register', data=json.dumps(self.create_user),content_type='application/json') 
+            self.client.post('/api/v1/register', data=json.dumps(self.create_user),         content_type='application/json') 
 
             login_data = {
                 "username": "jenny",
                 "password": "12345"
             }
             response = self.client.post('/api/v1/login', data=json.dumps(login_data),content_type='application/json')
-            self.assertEqual(response.status, '404 NOT FOUND')
+            self.assertEqual(response.status_code, 400)
             results = json.loads(response.data.decode())
             self.assertIn("Invaild username or password", results["error"])
             
@@ -94,39 +94,10 @@ class APITestCase(BaseTestCase):
     def test_get_one_incident(self):
         with self.client:
             self.client.post('/api/v1/register', data=json.dumps(self.create_user),content_type='application/json')
-            incident_data = {
-                "created_by": "jenny", 
-                "incident_type": "redflag",
-                "location": "bukoto",
-                "phone_number": "0726725271",
-                "status": "draft",
-                "images":"come.jpg",  
-                "videos":"go.png",
-                "comment":"fair"
-            }
-            response = self.client.post('/api/v1/incident', data=json.dumps(incident_data), content_type='application/json')
-            response = self.client.get('/api/v1/incident/2')
+            response = self.client.post('/api/v1/incident', data=json.dumps(self.create_incident), content_type='application/json')
+            response = self.client.get('/api/v1/incident/1')
             self.assertEqual(response.status_code, 200)
             self.assertIn("jenny", str(response.data))
-
-
-    def test_update_incident_location(self):
-        with self.client:
-            self.client.post('/api/v1/register', data=json.dumps(self.create_user),content_type='application/json')
-            incident_data = {
-                "created_by": "jenny", 
-                "incident_type": "redflag",
-                "location": "bukoto",
-                "phone_number": "0726725271",
-                "status": "draft",
-                "images":"come.jpg",  
-                "videos":"go.png",
-                "comment":"fair"
-            }
-            response = self.client.post('/api/v1/incident', data=json.dumps(incident_data), content_type='application/json')
-            response = self.client.patch('/api/v1/incident/2/location', data=json.dumps({'location':'Bombo'}), content_type='application/json')
-            self.assertEqual(response.status_code, 200)
-            self.assertIn("Bombo", str(response.data))
 
     def test_user_not_exits(self):
         with self.client:
@@ -151,7 +122,12 @@ class APITestCase(BaseTestCase):
         with self.client:
             response = self.client.post('/api/v1/incident', data=json.dumps(self.create_user),content_type='application/json')
             self.assertEqual(response.status_code, 400)
-            results = json.loads(response.data.decode())
+            self.assertIn("Please provide valid fields", str(response.data))  
+
+    def test_all_feilds_are_missing(self):
+        with self.client:
+            response = self.client.post('/api/v1/incident', data=json.dumps(self.required_feilds),content_type='application/json')
+            self.assertEqual(response.status_code, 400)
             self.assertIn("Please provide valid fields", str(response.data))  
             
     def test_get_specific_incident(self):
@@ -164,32 +140,46 @@ class APITestCase(BaseTestCase):
         with self.client:
             self.client.post('/api/v1/incident', data=json.dumps(self.create_user))
             response = self.client.get('/api/v1/incident', content_type='application/json')
-            self.assertEqual(response.status_code, 404)
+            self.assertEqual(response.status_code, 400)
 
     def test_no_incident_exits(self):
         with self.client:
             response = self.client.get('/api/v1/incident', content_type='application/json')
-            self.assertEqual(response.status_code, 404)
+            self.assertEqual(response.status_code, 400)
             results = json.loads(response.data.decode())
             self.assertIn("No incidents created", results["message"] )
             
     def test_update_location(self):
         with self.client:
-            incident_data = {
-                "location": "bukoto"
-            }
-            response =self.client.patch('/api/v1/2/location',data=json.dumps(incident_data), content_type='application/json')
-            self.assertEqual(response.status_code, 404)
+            response = self.client.post('/api/v1/register', data=json.dumps(self.create_user),content_type='application/json')
+            response = self.client.post('/api/v1/incident', data=json.dumps(self.create_incident),content_type='application/json')
+            response =self.client.patch('/api/v1/incident/1/location',data=json.dumps({'location':'location'}), content_type='application/json')
+            self.assertIn("Updated incident record", str(response.data))
+            self.assertEqual(response.status_code, 200)
+
+    def test_location_not_found(self):
+        with self.client:
+            response =self.client.post('/api/v1/register',data=json.dumps(self.create_user))
+            response =self.client.post('/api/v1/incident',data=json.dumps(self.create_incident), content_type='application/json')
+            response =self.client.patch('/api/v1/incident/1/location',data=json.dumps({}),content_type='application/json')
+            self.assertEqual(response.status_code, 400)
+            self.assertIn("Please include Location of the record", str(response.data))
+
 
     def test_update_comment(self):
         with self.client:
             incident_data = {
                 "comment": "Fair"
             }
-            response = self.client.patch('/api/v1/3/comment',data=json.dumps(incident_data), content_type='application/json')
-            self.assertEqual(response.status_code,404)
+            response =self.client.post('/api/v1/register',data=json.dumps(self.create_user))
+            response =self.client.post('/api/v1/incident',data=json.dumps(self.create_incident), content_type='application/json')
+            response = self.client.patch('/api/v1/incident/1/comment',data=json.dumps(incident_data), content_type='application/json')
+            self.assertEqual(response.status_code, 200)
 
     def test_delete_a_specific_incident(self):
         with self.client:
-            response = self.client.delete('/api/v1/incident/1', data=json.dumps(self.create_user), content_type='application/json')
-            self.assertEqual(response.status_code, 200)
+            self.client.post('/api/v1/incident', data=json.dumps(self.create_incident))
+            response = self.client.delete('/api/v1/incident/1', content_type='application/json')
+            self.assertEqual(response.status_code, 400)
+
+ 
